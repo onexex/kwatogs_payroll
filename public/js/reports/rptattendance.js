88ad5396675e1d1c1f65c334e6954d97f7205854
onexex <94344873+onexex@users.noreply.github.com>
2025-11-03 08:50:06 +0800

    changes-1132025-3

--- public/js/reports/rptattendance.js
+++ public/js/reports/rptattendance.js
@@ -50,9 +50,10 @@ document.addEventListener('DOMContentLoaded', function () {
                     //         <td colspan="9" class="text-start ps-2">${empName}</td>
                     //     </tr>
                     // `;
-                    const attendance = item.home_attendances?.[0] ?? {};
+          
                     // Employee’s Daily Records
                     records.forEach((item, index) => {
+                                  const attendance = item.home_attendances?.[0] ?? {};
                         function formatTime(timeString) {
                             if (!timeString) return '-';
                             const date = new Date(timeString);
